d02f7d7e5ae2902b9a87b3ad7bbff322d7144b3e
Yang Gao <imyanggao@gmail.com>
2016-02-26 13:51:09 -0500

    init commit

---
+++ init.el
@@ -0,0 +1,29 @@
+(message "init.el")
+
+;; add ~/.emacs.d/lisp/ to load path
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+
+;; disable automatic package loading, manually call function package-initialize in init-elpa.el
+(setq package-enable-at-startup nil)
+
+;; require all init-*, which are elisp under ~/.emacs.d/lisp/
+(require 'init-utils)                   ; some utilities' functions and macros, such as after-load
+(require 'init-elpa)                    ; elpa and define require-package function
+(require 'init-themes)                  ; themes
+(require 'init-smart-mode-line)         ; smart-mode-line
+(require 'init-nlinum)                  ; nlinum
+(require 'init-comment-dwim-2)          ; enhanced comment
+(require 'init-saveplace)               ; save place for reopen at same position
+(require 'init-yasnippet)               ; yasnippet
+(require 'init-auto-complete)           ; auto-complete
+(require 'init-projectile)              ; projectile
+(require 'init-helm)                    ; helm
+(require 'init-magit)                   ; magit
+(require 'init-lua)                     ; lua mode
+(require 'init-ggtags)                  ; GNU Global source code tagging system
+(require 'init-matlab)                  ; matlab mode
+(require 'init-auctex)                  ; auctex
+(require 'init-setup)                   ; preferences
+(require 'init-osdep)                   ; os dependent features
+
+(provide 'init)
