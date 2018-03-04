b3f9392d7dce2bda9d94a5d3eb09857d9ee08d1e
Yang <yang.gao.cs@gmail.com>
2018-03-04 03:06:45 -0800

    add two more theme

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -1,14 +1,27 @@
 (message "init-themes.el")
 
-(setq theme-choice 0)
+(setq theme-choice 1)
 
 (cond
  ((= theme-choice 1)
   (progn
-    ()))
- (t
+    (require-package 'monokai-theme)
+    (load-theme 'monokai t)
+    )
+  )
+ ((= theme-choice 2)
+  (progn
+    (require-package 'spaceline)
+    (require 'spaceline-config)
+    (spaceline-spacemacs-theme)
+    )
+  )
+ ((= theme-choice 3)
   (progn
     (require-package 'ample-zen-theme)
-    (load-theme 'ample-zen t))))
+    (load-theme 'ample-zen t)
+    )
+  )
+ )
 
 (provide 'init-themes)
