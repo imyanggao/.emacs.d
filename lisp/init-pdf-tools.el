ba5a9f78c3d2ddf73019d9e13e7c9c205b226cd8
Gao <ygaa@f0189814e39e.ant.amazon.com>
2019-04-29 09:36:49 -0700

    some dirty fix for pdf-tools

--- lisp/init-pdf-tools.el
+++ lisp/init-pdf-tools.el
@@ -2,6 +2,9 @@
 
 (require-package 'pdf-tools)
 
+;; dirty way to fix compiling issue: https://github.com/politza/pdf-tools/issues/480
+;; (setenv "PKG_CONFIG_PATH" "/usr/local/lib/pkgconfig:/usr/local/Cellar/libffi/3.2.1/lib/pkgconfig")
+
 ;; active the package
 (pdf-tools-install)
 
